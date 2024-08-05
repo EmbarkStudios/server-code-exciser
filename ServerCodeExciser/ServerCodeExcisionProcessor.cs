4943a324f03dad809f2aa91eb437060dad50b5b2
Arturo J. Pérez <arturo.perez@embark-studios.com>
2024-08-05 11:58:24 +0200

    Improve detection of already compliant code (#8)
    * Avoid creating dummy vars when they already exist
    
    * Multiple direction search

--- ServerCodeExciser/ServerCodeExcisionProcessor.cs
+++ ServerCodeExciser/ServerCodeExcisionProcessor.cs
@@ -215,6 +215,7 @@ namespace ServerCodeExcision
                     if (currentScope.StartIndex == -1
                         || currentScope.StopIndex == -1
                         || InjectedMacroAlreadyExistsAtLocation(answerText, currentScope.StartIndex, true, excisionLanguage.ServerScopeStartString)
+                        || InjectedMacroAlreadyExistsAtLocation(answerText, currentScope.StartIndex, false, excisionLanguage.ServerScopeStartString)
                         || InjectedMacroAlreadyExistsAtLocation(answerText, currentScope.StopIndex, false, excisionLanguage.ServerScopeEndString))
                     {
                         continue;
@@ -230,13 +231,21 @@ namespace ServerCodeExcision
                 // Next we must add dummy reference variables if they exist.
                 foreach (KeyValuePair<int, HashSet<string>> dummyRefDataPair in visitor.ClassStartIdxDummyReferenceData)
                 {
-                    var dummyRefDataBlockString = new StringBuilder("#ifndef " + excisionLanguage.ServerPrecompilerSymbol);
+                    var dummyRefDataBlockString = new StringBuilder();
+                    var dummyVarScope = "#ifndef " + excisionLanguage.ServerPrecompilerSymbol;
+                    dummyRefDataBlockString.Append(dummyVarScope);
                     foreach (var dummyVarDef in dummyRefDataPair.Value)
                     {
                         dummyRefDataBlockString.Append("\r\n\t" + dummyVarDef);
                     }
 
                     dummyRefDataBlockString.Append("\r\n" + excisionLanguage.ServerScopeEndString + "\r\n");
+
+                    // If there is already a block of dummy reference variables we skip adding new ones, there is no guarantee we are adding the right code.
+                    if (InjectedMacroAlreadyExistsAtLocation(answerText, dummyRefDataPair.Key, false, dummyVarScope + "\r\n"))
+                    {
+                        continue;
+                    }
 
                     serverCodeInjections.Add(new KeyValuePair<int, string>(dummyRefDataPair.Key, dummyRefDataBlockString.ToString()));
                 }
